b2bde0d9eb2fe9690108589f9f8e81f145e5e651
Brad Davidson <brad.davidson@rancher.com>
2025-07-02 19:19:34 +0000

    Add pregenerated CRD manifests
    Will replace the use of wrangler CRD helpers at
    https://github.com/k3s-io/k3s/blob/338b9cc923dbf93b320a8420e5b1355556671efb/pkg/crd/crds.go#L12-L22
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit 7abd92d28697630288f7c22636e36ea78cb6596d)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 //go:generate go run pkg/codegen/cleanup/main.go
-//go:generate rm -rf pkg/generated
+//go:generate rm -rf pkg/generated pkg/crds/yaml/generated
 //go:generate go run pkg/codegen/main.go
+//go:generate controller-gen crd:generateEmbeddedObjectMeta=true paths=./k3s.cattle.io/... output:crd:dir=./pkg/crds/yaml/generated
 
 package main
 
